1644c6f59ca12b51540a5f57fa18ecff354f8ba3
jujulabz <mp678503@gmail.com>
2026-05-03 21:26:40 -0400

    adding in imports

---
+++ Lab15_jujulabz#1.py
@@ -0,0 +1,18 @@
+""""" 
+Plot a math formula
+
+Melissa Palmer
+
+This program plots a sine wave using matplotlib.
+
+5/1/2026
+
+"""
+
+import matplotlib
+
+matplotlib.use("Agg")
+
+import matplotlib.pyplot as plt
+import math
+
